c47e917d10a51c8c11453a9514ab273bf44fb275
Alisson Lima <alissonlimabr.github@gmail.com>
2025-01-21 11:17:11 -0500

    feat: projeto base em gradle

---
+++ src/test/java/dev/alissonlimabr/smssender/SmsSenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alissonlimabr.smssender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsSenderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
